33ab40ce9cd18ced5e22323e6e63af97a476cad2
Edoardo Tenani <edoardo.tenani@gmail.com>
2015-09-07 19:20:45 +0200

    add ngrun

---
+++ templates/ngrun.js
@@ -0,0 +1,22 @@
+(function (angular) {
+  'use strict'
+  // https://github.com/johnpapa/angular-styleguide#run-blocks
+
+  /**
+    $1
+
+    Run configurations for $1
+   */
+
+  angular
+    .module('$1')
+    .run(Run$1)
+
+  /* @ngInject */
+  function Run$1 ($2) {
+
+    $3
+
+  }
+
+}(window.angular))
