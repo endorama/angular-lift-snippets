73dba673842c0f3de9424c31cb2135e715bd71cd
Edoardo Tenani <edoardo.tenani@gmail.com>
2015-09-07 19:19:15 +0200

    update ngconfig

--- templates/ngconfig.js
+++ templates/ngconfig.js
@@ -1,18 +1,19 @@
 (function (angular) {
   'use strict'
-
+  // https://github.com/johnpapa/angular-styleguide#configuration-1
+  
   /**
     $1
 
-    Startup configurations for $1
+    Startup configurations for $2
    */
 
   angular
     .module('$1')
-    .config($2)
+    .config($2_Configuration)
 
   /* @ngInject */
-  function $2 ($3) {
+  function $2_Configuration ($3) {
 
     $4
 
